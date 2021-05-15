e5fb9478b70ccda018f2277e09c9782571861b75
Shashanka <shashanka.panda@gmail.com>
2021-05-15 10:55:22 +0530

    updated config

--- config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host:"https://nobhubcms.azurewebsites.net",
-  port: "${process.env.PORT || 1337}",
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'a16a9806f89311bf1c1a7d2b6f756d4e'),
-    },
-  },
-});
